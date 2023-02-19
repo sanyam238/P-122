586e3ff2a9fc79b1f4c82d454867c2d15e8c1830
sanyam238 <82820362+sanyam238@users.noreply.github.com>
2023-02-19 14:56:02 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ recognition.onresult = function(event) {
 function setup() {
  screen_width = window.innerWidth;
  screen_height = window.innerHeight;
- createCanvas(screen_width,screen_height - 150);
+ canvas = createCanvas(screen_width,screen_height - 150);
  canvas.position(0,150);
 }
 
